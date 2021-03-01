519b0b27ebc7c57f8bff501e63f38eb61b51229a
RafaelVendrameto <rafaelvendrameto@gmail.com>
2021-03-01 19:53:05 -0300

    arquivo com metodo main

---
+++ src/main/java/com/omni/projetosomni/ProjetosOmniApplication.java
@@ -0,0 +1,13 @@
+package com.omni.projetosomni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosOmniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosOmniApplication.class, args);
+	}
+
+}
