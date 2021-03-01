67c1e84be2af94ba00cf987883fbb9f2a2b10a3c
RafaelVendrameto <rafaelvendrameto@gmail.com>
2021-03-01 19:53:47 -0300

    repository de projetos para banco de dados

---
+++ src/main/java/com/omni/projetosomni/repository/ProjetoRepository.java
@@ -0,0 +1,8 @@
+package com.omni.projetosomni.repository;
+
+import com.omni.projetosomni.model.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjetoRepository extends JpaRepository<Projeto, Integer> {
+
+}
