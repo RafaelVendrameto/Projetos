cca997e91c9c8013f67d3a5c1de6556b7f79164d
RafaelVendrameto <rafaelvendrameto@gmail.com>
2021-03-01 19:54:05 -0300

    classe de testes

---
+++ src/test/java/com/omni/projetosomni/ProjetosOmniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omni.projetosomni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosOmniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
